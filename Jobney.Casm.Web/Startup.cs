911f6a303f4793c52894b27fec5e0becce4a71b3
Justin Obney <justinobney@gmail.com>
2013-12-31 17:45:44 -0600

    ASP MVC 5 - Default Site

---
+++ Jobney.Casm.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jobney.Casm.Web.Startup))]
+namespace Jobney.Casm.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
