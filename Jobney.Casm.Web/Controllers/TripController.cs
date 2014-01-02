3cb9536f94e057ca2b45e9d2e38c833ad80b34f2
Justin Obney <justinobney@gmail.com>
2014-01-01 22:43:43 -0600

    scaffolding out trip info page

---
+++ Jobney.Casm.Web/Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Jobney.Casm.Web.Controllers
+{
+    public class TripController : Controller
+    {
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
