489c3f174e9aa6d8ededb008d6fcea36e775026b
Justin Obney <justinobney@gmail.com>
2014-01-04 13:53:34 -0600

    scaffolding Manage App routes and Bundle Setup

---
+++ Jobney.Casm.Web/Scripts/apps/manage/manage-app-ctrl.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('Jobney.Casm.ManageApp');
+
+    app.controller('ManageAppCtrl', ['$scope', '$state', function ($scope, $state) {
+        $scope.activeClass = function(state) {
+            return $state.is(state) ? 'active' : '';
+        };
+    }]);
+})();
