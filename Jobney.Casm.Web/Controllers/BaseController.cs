316fd1590beb363dd6bec226af401dfa8d573175
Justin Obney <justinobney@gmail.com>
2014-01-03 22:24:53 -0600

    Adding BaseController and JsonNetResult

---
+++ Jobney.Casm.Web/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Jobney.Casm.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected internal JsonNetResult JsonResult(object data)
+        {
+            return new JsonNetResult
+            {
+                Data = data,
+                Formatting = Formatting.Indented,
+                SerializerSettings = new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                }
+            };
+        }
+    }
+}
--- Jobney.Casm.Web/Controllers/HomeController.cs
+++ Jobney.Casm.Web/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Jobney.Casm.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
