71bead97e78f4c488cb2c4fb4a0e473b9d0c6ed5
Justin Obney <justinobney@gmail.com>
2014-01-03 22:53:38 -0600

    Adding Domain Project

---
+++ Jobney.Casm.Domain/PersonBase.cs
@@ -0,0 +1,9 @@
+using Jobney.Core.Domain;
+
+namespace Jobney.Casm.Domain
+{
+    public class PersonBase : Entity
+    {
+
+    }
+}
---
+++ Jobney.Casm.Domain/Pilot.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobney.Casm.Domain
+{
+    public class Pilot:PersonBase
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
