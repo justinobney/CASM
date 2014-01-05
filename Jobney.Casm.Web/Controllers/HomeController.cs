a0fef4f56c89eea0c14e0c1bf9bcf24791a33141
Justin Obney <justinobney@gmail.com>
2014-01-05 00:25:31 -0600

    Cleaning up to begin implementing UI functionality

--- Jobney.Casm.Web/Controllers/HomeController.cs
+++ Jobney.Casm.Web/Controllers/HomeController.cs
@@ -1,42 +1,21 @@
 ﻿using System.Web.Mvc;
 using Jobney.Casm.Domain;
-using Jobney.Casm.Web.Helpers;
 using Jobney.Core.Domain.Interfaces;
 
 namespace Jobney.Casm.Web.Controllers
 {
     public class HomeController : BaseController
     {
-        private readonly IRepository<Pilot> pilotRepository;
+        private readonly IRepository<Trip> tripRepository;
 
-        public HomeController(IRepository<Pilot> pilotRepository)
+        public HomeController(IRepository<Trip> tripRepository)
         {
-            this.pilotRepository = pilotRepository;
+            this.tripRepository = tripRepository;
         }
 
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public JsonNetResult Pilots()
-        {
-            var pilots = pilotRepository.GetAll();
-            return JsonResult(pilots);
         }
     }
 }
--- Jobney.Casm.Web/Controllers/TripController.cs
+++ Jobney.Casm.Web/Controllers/TripController.cs
@@ -1,9 +1,18 @@
 ﻿using System.Web.Mvc;
+using Jobney.Casm.Domain;
+using Jobney.Core.Domain.Interfaces;
 
 namespace Jobney.Casm.Web.Controllers
 {
-    public class TripController : Controller
+    public class TripController : BaseController
     {
+        private readonly IRepository<Trip> tripRepository;
+
+        public TripController(IRepository<Trip> tripRepository )
+        {
+            this.tripRepository = tripRepository;
+        }
+
         public ActionResult Info()
         {
             return View();
