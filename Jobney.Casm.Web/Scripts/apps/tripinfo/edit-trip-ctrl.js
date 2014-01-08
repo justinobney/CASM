0386287e2e954c50ec61b1d6e843b638120ffad9
Justin Obney <jobney@envoc.com>
2014-01-07 21:48:06 -0600

    scaffolding out layout for CASM trip info

--- Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
+++ Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
@@ -6,11 +6,16 @@
     app.controller('EditTripCtrl', ['$scope', '$stateParams', 'TripService',
         function ($scope, $stateParams, TripService) {
 
+            $scope.handleRowClick = function(open) {
+                $scope.min = true;
+                return !open;
+            };
+
             activate();
 
             function activate() {
                 TripService.getById($stateParams.id).then(function(trip) {
-                    $scope.data = trip;
+                    $scope.trip = trip;
                 });
             }
 
