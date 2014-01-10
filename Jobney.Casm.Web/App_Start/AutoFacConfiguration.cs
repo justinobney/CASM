feda6ec0cc1a451a876998d543dcb04a554d067b
Justin Obney <justinobney@gmail.com>
2014-01-10 00:05:31 -0600

    Autofac Cleanup

--- Jobney.Casm.Web/App_Start/AutoFacConfiguration.cs
+++ Jobney.Casm.Web/App_Start/AutoFacConfiguration.cs
@@ -1,13 +1,5 @@
-using System.Reflection;
-using System.Security.Principal;
-using System.Web;
 using Autofac;
-using Autofac.Integration.Mvc;
 using Jobney.Casm.Data.Configuration;
-using Jobney.Casm.Web.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Module = Autofac.Module;
 
 namespace Jobney.Casm.Web
 {
@@ -22,28 +14,6 @@ namespace Jobney.Casm.Web
         {
             builder.RegisterModule<DataModule>();
             builder.RegisterModule<WebModule>();
-        }
-    }
-}
-
-namespace Jobney.Casm.Web
-{
-    public class WebModule : Module
-    {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.Register(c => HttpContext.Current.User.Identity)
-                .As<IIdentity>()
-                .InstancePerLifetimeScope();
-
-            builder.Register(c => new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
-                .As<UserManager<ApplicationUser>>()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterControllers(Assembly.GetExecutingAssembly());
-            
-            builder.RegisterType(typeof(UserValidator<>));
-
         }
     }
 }
