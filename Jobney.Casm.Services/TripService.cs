75c345e6e940984315face5f7260fca0c7b259b4
Justin Obney <justinobney@gmail.com>
2014-01-10 19:29:24 -0600

    Adding tests proj and test-driving TripService Waypoint Reorder

---
+++ Jobney.Casm.Services/TripService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Jobney.Casm.Domain;
+
+namespace Jobney.Casm.Services
+{
+    public class TripService
+    {
+        public void ReorderWaypoints(Trip trip, int wpId, int newOrder)
+        {
+            var changingWaypoints = trip.Waypoints.Where(w => w.Id != wpId);
+            var originalOrder = trip.Waypoints.FirstOrDefault(w => w.Id == wpId).Order;
+
+            // Handle New Order Out Of Range
+            if (newOrder > trip.Waypoints.Max(w => w.Order))
+                newOrder = trip.Waypoints.Max(w => w.Order);
+
+            // Set order of waypoint in question
+            trip.Waypoints.FirstOrDefault(w => w.Id == wpId).Order = newOrder;
+
+
+            ReorderTopGap(newOrder, changingWaypoints, originalOrder);
+            ReorderBottomGap(newOrder, changingWaypoints, originalOrder);
+        }
+
+        private void ReorderBottomGap(int newOrder, IEnumerable<Waypoint> changingWaypoints, int originalOrder)
+        {
+            changingWaypoints = changingWaypoints.Where(w => w.Order <= newOrder && w.Order > originalOrder);
+
+            foreach (var wp in changingWaypoints)
+            {
+                wp.Order = wp.Order - 1;
+            }
+        }
+
+        private static void ReorderTopGap(int newOrder, IEnumerable<Waypoint> changingWaypoints, int originalOrder)
+        {
+            changingWaypoints = changingWaypoints.Where(w => w.Order >= newOrder && w.Order < originalOrder);
+
+            foreach (var wp in changingWaypoints)
+            {
+                wp.Order = wp.Order + 1;
+            }
+        }
+    }
+}
---
+++ Jobney.Casm.Test/TripServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Jobney.Casm.Domain;
+using Jobney.Casm.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobney.Casm.Tests
+{
+    [TestClass]
+    public class TripServiceTests
+    {
+
+        private TripService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            service = new TripService();
+        }
+
+        [TestMethod]
+        public void ReorderTripWaypointsSetsWaypointOrder()
+        {
+            var trip = TestDataGenerator.GetTrip(); // Should return trip w/ 3 waypoints
+            trip.Waypoints.ToArray()[0].Order = 1;
+            trip.Waypoints.ToArray()[1].Order = 2;
+            trip.Waypoints.ToArray()[2].Order = 3;
+
+            var waypointInQuestion = trip.Waypoints.ToArray()[1];
+            var newIndex = 1;
+
+            service.ReorderWaypoints(trip, waypointInQuestion.Id, newIndex);
+
+            Assert.AreEqual(waypointInQuestion.Order, newIndex);
+        }
+
+        [TestMethod]
+        public void ReorderTripWaypointsReordersWhenSetToFirst()
+        {
+            var trip = TestDataGenerator.GetTrip(); // Should return trip w/ 3 waypoints
+            trip.Waypoints.ToArray()[0].Order = 1;
+            trip.Waypoints.ToArray()[1].Order = 2;
+            trip.Waypoints.ToArray()[2].Order = 3;
+
+            var waypointInQuestion = trip.Waypoints.ToArray()[1];
+
+            service.ReorderWaypoints(trip, waypointInQuestion.Id, 1);
+
+            Assert.AreEqual(trip.Waypoints.ToArray()[0].Order, 2);
+            Assert.AreEqual(trip.Waypoints.ToArray()[1].Order, 1);
+            Assert.AreEqual(trip.Waypoints.ToArray()[2].Order, 3);
+        }
+
+        [TestMethod]
+        public void ReorderTripWaypointsReordersWhenSetToLast()
+        {
+            var trip = TestDataGenerator.GetTrip(); // Should return trip w/ 3 waypoints
+            trip.Waypoints.ToArray()[0].Order = 1;
+            trip.Waypoints.ToArray()[1].Order = 2;
+            trip.Waypoints.ToArray()[2].Order = 3;
+
+            var waypointInQuestion = trip.Waypoints.ToArray()[1];
+
+            service.ReorderWaypoints(trip, waypointInQuestion.Id, 3);
+
+            Assert.AreEqual(trip.Waypoints.ToArray()[0].Order, 1);
+            Assert.AreEqual(trip.Waypoints.ToArray()[1].Order, 3);
+            Assert.AreEqual(trip.Waypoints.ToArray()[2].Order, 2);
+        }
+
+        [TestMethod]
+        public void ReorderTripWaypointsHandleOutOfRangeReorder()
+        {
+            var trip = TestDataGenerator.GetTrip(); // Should return trip w/ 3 waypoints
+            trip.Waypoints.ToArray()[0].Order = 1;
+            trip.Waypoints.ToArray()[1].Order = 2;
+            trip.Waypoints.ToArray()[2].Order = 3;
+
+            var waypointInQuestion = trip.Waypoints.ToArray()[1];
+
+            service.ReorderWaypoints(trip, waypointInQuestion.Id, 6);
+
+            Assert.AreEqual(trip.Waypoints.ToArray()[0].Order, 1);
+            Assert.AreEqual(trip.Waypoints.ToArray()[1].Order, 3);
+            Assert.AreEqual(trip.Waypoints.ToArray()[2].Order, 2);
+        }
+    }
+}
