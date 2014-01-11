73e00841dbf053876803db007323b74a05a773da
Justin Obney <justinobney@gmail.com>
2014-01-11 00:05:47 -0600

    implementing passenger select2

--- Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
+++ Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
@@ -21,19 +21,29 @@
                         setTimeout(function() {
                             var waypointId = ui.item.scope().location.id;
                             var newOrder = ui.item.index() + 1;
-                            var tripId = $scope.trip.id;
-
-                            TripService.ReorderWaypoint(tripId, waypointId, newOrder).then(function(response) {
-                                _.each($scope.trip.waypoints, function (wp, idx) {
-                                    wp.order = _.findWhere(response.tripOrderMap, { id: wp.id }).order;
-                                });
-                            });
+                            handleWaypointReorder(waypointId, newOrder);
                         }, 0);
                     },
                     revert: true,
                     handle: ".drag-handle"
                 };
+
+                $scope.passengerList = _.map(BootstrappedData.passengers, function(p) {
+                    return { id: p.id, text: p.firstName + ' ' + p.lastName };
+                });
+
+                $scope.select2Options = {};
             }
+
+            function handleWaypointReorder(waypointId, newOrder) {
+                var tripId = $scope.trip.id;
+
+                TripService.ReorderWaypoint(tripId, waypointId, newOrder).then(function (response) {
+                    _.each($scope.trip.waypoints, function (wp, idx) {
+                        wp.order = _.findWhere(response.tripOrderMap, { id: wp.id }).order;
+                    });
+                });
+            };
 
         }
     ]);
