9ff9135accdbbb37a0412d74218276cd580067b9
Justin Obney <justinobney@gmail.com>
2014-01-11 00:57:27 -0600

    adding map to page

--- Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
+++ Jobney.Casm.Web/Scripts/apps/tripinfo/edit-trip-ctrl.js
@@ -33,6 +33,17 @@
 
                 $scope.select2Options = {};
                 $scope.ngAutocompleteOptions = {};
+                $scope.tripMapOptions = {
+                    center: new google.maps.LatLng(30.450, -91.140),
+                    panControl: false,
+                    zoomControl: false,
+                    mapTypeControl: false,
+                    overviewMapControl: false,
+                    rotateControl: false,
+                    scaleControl: false,
+                    streetViewControl: false,
+                    styles: [{ "featureType": "water", "stylers": [{ "visibility": "on" }, { "color": "#acbcc9" }] }, { "featureType": "landscape", "stylers": [{ "color": "#f2e5d4" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#c5c6c6" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#e4d7c6" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#fbfaf7" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#c5dac6" }] }, { "featureType": "administrative", "stylers": [{ "visibility": "on" }, { "lightness": 33 }] }, { "featureType": "road" }, { "featureType": "poi.park", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "lightness": 20 }] }, {}, { "featureType": "road", "stylers": [{ "lightness": 20 }] }]
+                };
 
                 setupWatches();
             }
