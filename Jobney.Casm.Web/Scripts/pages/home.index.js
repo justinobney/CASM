e80f14a1c313b6c917815289a979c4657aa22b39
Justin Obney <justinobney@gmail.com>
2014-01-11 22:16:23 -0600

    refactoring to have page specific javascript that is not an angular app in a "pages" folder

---
+++ Jobney.Casm.Web/Scripts/pages/home.index.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    'use strict';
+
+    $(document).on('click', '[data-action="add-trip"]', function (e) {
+        //code here
+        calendar
+            .fullCalendar('renderEvent',
+                {
+                    title: $('#NewTripTitle').val(),
+                    start: $('#NewTripDate').val()
+                },
+                true // make the event "stick"
+            );
+    });
+
+    $('#NewTripDate').datepicker({});
+})()
