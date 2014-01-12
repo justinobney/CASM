6d6c3364162df6644f6c5d931849ad77d14271e4
Justin Obney <justinobney@gmail.com>
2014-01-11 22:30:01 -0600

    refactoring home page into it's own app

--- Jobney.Casm.Web/Scripts/pages/home.index.js
+++ Jobney.Casm.Web/Scripts/pages/home.index.js
@@ -1,17 +1,38 @@
 ﻿(function() {
     'use strict';
+    var homeApp = window.homeApp = {};
 
-    $(document).on('click', '[data-action="add-trip"]', function (e) {
-        //code here
-        calendar
-            .fullCalendar('renderEvent',
-                {
-                    title: $('#NewTripTitle').val(),
-                    start: $('#NewTripDate').val()
-                },
-                true // make the event "stick"
-            );
-    });
+    homeApp.init = function (events) {
+        homeApp.events = events;
+        bindEvents();
+        initCalendar();
+        $('#NewTripDate').datepicker({});
+    };
 
-    $('#NewTripDate').datepicker({});
+    function initCalendar() {
+        homeApp.calendar = $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: ''
+            },
+            selectable: true,
+            selectHelper: true,
+            events: homeApp.events
+        });
+    }
+
+    function bindEvents() {
+        $(document).on('click', '[data-action="add-trip"]', function (e) {
+            //code here
+            homeApp.calendar
+                .fullCalendar('renderEvent',
+                    {
+                        title: $('#NewTripTitle').val(),
+                        start: $('#NewTripDate').val()
+                    },
+                    true // make the event "stick"
+                );
+        });
+    };
 })()
